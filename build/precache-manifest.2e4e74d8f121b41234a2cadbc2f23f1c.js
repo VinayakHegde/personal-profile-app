a879ee8b4acd317df18c2528e27e8036fd966b90
Vinayak Hegde <vinayak.nandi@gmail.com>
2019-03-31 23:01:49 +0100

    Build folder

---
+++ build/precache-manifest.2e4e74d8f121b41234a2cadbc2f23f1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "11f43137a23829838584",
+    "url": "/static/js/main.e2a8146a.chunk.js"
+  },
+  {
+    "revision": "3f4ade220ee875194238",
+    "url": "/static/js/2.7b23cc03.chunk.js"
+  },
+  {
+    "revision": "11f43137a23829838584",
+    "url": "/static/css/main.1c3f4a48.chunk.css"
+  },
+  {
+    "revision": "153519a715933bc1558661bc826fcb7c",
+    "url": "/index.html"
+  }
+];
